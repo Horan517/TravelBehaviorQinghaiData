2c8cbd37c04b0c81a8ad2957bc9905638c7a9a2a
Horan517 <haoranx517@gmail.com>
2024-12-21 15:15:41 -0500

    commit after lunch

---
+++ R/TravelBehaviorQinghaiData-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' TravelBehaviorQinghaiData: Data for the paper "Assessing the influence of tourists’ perceived travel
+#' environment and their travel behavior on travel satisfaction using structural equation models (SEM):
+#' a case of Qinghai-Tibet Plateau".
+#'
+#'
+#' @docType package
+#' @name TravelBehaviorQinghaiData-package
+#' @author Haoran Xu, School of Earth, Environment and Society, McMaster University \email{xu976@@mcmaster.ca}
+#' @references \url{https://github.com/Horan517/TravelBehaviorQinghaiData}
+NULL
